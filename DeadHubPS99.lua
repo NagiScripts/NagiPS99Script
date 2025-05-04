8aeae03209e9b08f9249897da0216bcd20a0456f
redzhubreal <sigma123alpha123beta123@gmail.com>
2025-05-04 14:17:18 +0530

    Update DeadHubPS99.lua

--- DeadHubPS99.lua
+++ DeadHubPS99.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/UuTSY9a9/raw"))()
+--loadstring(game:HttpGet("https://pastefy.app/UuTSY9a9/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/1By4tPP0/raw"))()
